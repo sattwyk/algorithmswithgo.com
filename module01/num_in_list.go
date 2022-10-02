70767a116de967cc83c5ba6ca73e678577a0a126
Sattwik <hellosattwik@gmail.com>
2022-10-02 21:09:36 +0530

    NumInList solved 01

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -8,8 +8,16 @@ func NumInList(list []int, num int) bool {
 		return false
 	}
 
-	for i := 0; i < len(list); i++ {
-		if list[i] == num {
+	// SOLUTION: 1
+	// for i := 0; i < len(list); i++ {
+	// 	if list[i] == num {
+	// 		return true
+	// 	}
+	// }
+
+	// SOLUTION 2
+	for _, val := range list {
+		if val == num {
 			return true
 		}
 	}
