cefcbb5e2f6eee4a280448cbf628e66f8f581bf7
Sattwik <hellosattwik@gmail.com>
2022-10-02 21:04:32 +0530

    NumInList solved

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -3,10 +3,16 @@ package module01
 // NumInList will return true if num is in the
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
+
+	if len(list) == 0 {
+		return false
+	}
+
 	for i := 0; i < len(list); i++ {
 		if list[i] == num {
 			return true
 		}
 	}
+
 	return false
 }
